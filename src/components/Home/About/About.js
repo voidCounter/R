a90018bb0f0836f91e47c0586c6c23a0613dac5f
rohittz <r_o_h_i_t_roy@yahoo.com>
2021-05-05 16:15:42 +0600

    about me added

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import userImage from '../../../images/userImage-removebg.png';
+import { useState } from 'react';
+import userImage from '../../../images/userImage-removebg.jpg';
 import aboutStyles from './About.module.css';
 const About = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
     return (
         <div className={aboutStyles.aboutContainer}>
-            <div className={aboutStyles.about}>
-                <div className={aboutStyles.userImage}>
-                    <img src={userImage} alt="Rohit Roy" />
-                </div>
-                <div className={aboutStyles.aboutDescription}>
+            <div className={`${aboutStyles.about} about`}>
 
+                <div className={`${aboutStyles.userImageContainer} userImageContainer`}>
+                    {isLoaded ? null : (
+                        <div
+                            style={{
+                                backgroundColor: 'var(--text-gray)',
+                                height: '100%',
+                                width: '100%'
+                            }}
+                        />
+                    )}
+                    <div className={aboutStyles.userImage}>
+                        <img style={isLoaded ? {} : { display: 'none' }} onLoad={() => setIsLoaded(true)} src={userImage} alt="Rohit Roy" /></div>
+                </div>
+
+                <div className={`${aboutStyles.aboutDescription} aboutDescription`}>
+                    Bonjour! My name is Rohit Roy. At the time of reading 10th standard, I came to know about the existance of some technologies used to create this internet.
                 </div>
             </div>
         </div>
