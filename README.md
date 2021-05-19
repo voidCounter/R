d1e32d44125dc7e9651e84b5697e612fe7b6e35b
rohittz <r_o_h_i_t_roy@yahoo.com>
2021-05-19 14:24:22 +0600

    updated projects section

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 <img src="/src/images/SS/full-page.jpg" width="100%" height="auto" align="center">
+<br/>
--- src/components/Home/Projects/ProjectsData.js
+++ src/components/Home/Projects/ProjectsData.js
@@ -5,13 +5,16 @@ import fcfutaleufu from '../../../images/projects/fc-futaleufu.jpg';
 const projectsData = [
     {
         title: 'AAron-Stanley: A photographer portfolio',
-        frontEnd: ['React.js', 'HTML5', 'CSS3', 'BootStrap'],
+        frontEnd: ['React.js', 'HTML5', 'CSS3', 'BootStrap', 'Material UI'],
         backEnd: ['MongoDB', 'FireBase', 'Express.js'],
         features: [
-            'Built React application for photographers to maintain cilents with ease.',
-            'Implemented a firebase authentication system.',
-            'Implemented admin dashboard to regulate clients’ bookings and stripe payment method.',
-            'Used React, CSS3, JavaScript, Material UI for creating radiant front-end design.',
+            "A photography service management website built with React.js, Express.js and MongoDB.",
+            "Used React, Material UI, CSS3 to create the stunning UI.",
+            "Separate admin and customer dashboard have been implemented. Hence Customers can't enter the admin dashboard.",
+            'Implemented a firebase authentication system. The Dashboard will divert automatically after getting logged in.',
+            "In the admin dashboard, an admin can regulate the customers.' orders, make anyone admin using email, add new services on the UI and manage existing services dynamically.",
+            'In the user dashboard, a user can buy existing services, track the orders and give reviews dynamically.',
+            'The Stripe payment method has been implemented in the user dashboard.',
             'Used Express, MongoDB for back-end development.',
             'Deployed to firebase and Heroku is functioning at the backend.'
         ],
@@ -24,11 +27,12 @@ const projectsData = [
         frontEnd: ['React.js', 'HTML5', 'CSS3', 'BootStrap'],
         backEnd: ['MongoDB', 'FireBase', 'Express.js'],
         features: [
-            'An online bookstore built with React and Express.',
-            'Implemented a firebase authentication system to deal with customers’ orders.',
-            'Implemented admin dashboard to regulate clients’ bookings and stripe payment method.',
-            'Implemented admin dashboard to manage available books in the store.',
-            'Used React, CSS3 for designing the website.',
+            'An online bookstore built with React.js and Express.js.',
+            'Implemented a firebase authentication system to manage the customers.',
+            "No separate admin dashboard hasn't been created. Users have access to the common dashboard.",
+            'Users can manage the existing books in the store. They can delete books and add new books.',
+            'All the books data is being stored in MongoDB. They are being loaded based on requests. ',
+            'Users can see their past ordered books in the orders section.',
             'Deployed to firebase and Heroku is operating at the back-end.'
         ],
         github: 'https://github.com/rohittz/bookscaper',
@@ -40,11 +44,11 @@ const projectsData = [
         frontEnd: ['HTML', 'CSS3', 'JavaScript'],
         backEnd: ['JavaScript'],
         features: [
-            'A simple UI created with pure CSS3 and Vanilla.js',
-            'KeyPress event added to add todos flawlessly',
-            'Done and Delete option added to modify a single todo',
-            'A pregress bar is added to track the remaining todos',
-            'Option to change UI color has been added'
+            'A simple UI created with pure CSS3 and Vanilla.js.',
+            'KeyPress event added to add todos flawlessly.',
+            'Done and Delete option added to modify a single todo.',
+            'A progress bar is added to track the remaining todos.',
+            'Option to change UI colour has been added.'
 
         ],
         github: 'https://github.com/rohittz/some-todo',
@@ -56,11 +60,11 @@ const projectsData = [
         frontEnd: ['React.js', 'CSS3', 'BootStrap'],
         backEnd: ['JavaScript'],
         features: [
-            'A simple UI created with pure CSS3 and Vanilla.js',
+            'A simple UI created with pure CSS3 and Vanilla.js.',
             'Used Realistic images of the players.',
-            'Images are hosted at imgbb website.',
+            'Images are hosted at the imgbb website.',
             'Users can hire any player, the price is shown at a fixed display at the bottom of the viewport.',
-            'Responsiveness added using CSS3 media-queries.'
+            'Responsiveness added using CSS3 media queries.'
 
         ],
         github: 'https://github.com/rohittz/fc-futaleufu',
